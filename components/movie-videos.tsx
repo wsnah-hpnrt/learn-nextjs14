3a094e8f3bf72f1d4e6fcb3a8241109ddc9fd1d9
wsnah-hpnrt <wsnah@hpnrt.co.kr>
2025-03-25 15:04:22 +0900

    3.5 Deployment

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,16 +1,29 @@
 // 여기는 videos에 관한 UI만 가질꺼임
 
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  throw new Error("something broke...");
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error("something broke...");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,13 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
-  const [count, setCount] = useState(0);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "🧨" : ""}
@@ -16,9 +15,6 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "🧨" : ""}
-        </li>
-        <li>
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
